e6f72b5d4861bea4195bdb60e32a8310b254b062
Evan Hennis <nurl_50@hotmail.com>
2017-07-05 14:28:56 -0500

    Finishing touches on the graphs.

---
+++ KArmedBandit/KArmedBandit/Bandit.py
@@ -0,0 +1,43 @@
+import numpy as np
+import math
+
+class Bandit(object):
+    """Bandit Class"""
+    def __init__(self, arms, epsilon):
+        self.arms = arms
+        self.K = np.zeros(arms)
+        self.epsilon = epsilon
+        self.estimate_values = np.zeros(arms) #Initialize to 0 at the start
+        self.q_star = np.random.normal(0,math.sqrt(1),arms) #Normal distribution with mean 0 and variance 1
+        self.optimal_action = np.argmax(self.q_star)
+        
+
+    def get_arm(self):
+        #Randomly determine if we are going to explore or exploit
+        if np.random.random() > self.epsilon:
+            #Exploit
+            return np.argmax(self.estimate_values)
+        else:
+            #Explore
+            return np.random.randint(0,self.arms)
+
+    def get_reward(self, arm):
+        #Normal (Guassian) probabilty of mean Q* and variance 1
+        reward = np.random.normal(self.q_star[arm],math.sqrt(1)) 
+        return reward
+
+    def get_average_reward(self):
+        # Grab the max value from the arms
+        return np.max(self.estimate_values)
+
+    def update_estimate_values(self, arm, reward):
+        self.K[arm] += 1
+        k = self.K[arm]
+        #Set new reward (r1 + r2 ... ra)/ka
+        current_total = self.estimate_values[arm]
+        #(k-1/k)* current_total + (1/k) * reward
+        self.estimate_values[arm] = ((k-1) / float(k)) * current_total + (1/float(k)) * reward
+
+    def best_action(self, arm):
+        return self.optimal_action == arm
+
--- KArmedBandit/KArmedBandit/KArmedBandit.py
+++ KArmedBandit/KArmedBandit/KArmedBandit.py
@@ -1,2 +1,54 @@
 #K-Armed Bandit
 #http://incompleteideas.net/sutton/book/ebook/node16.html
+import numpy as np
+import matplotlib.pyplot as plt
+from Bandit import *
+
+def process_bandit(epsilon, bandit_count, plays):
+    optimal_action = np.zeros(plays)
+    average_reward = np.zeros(plays)
+    for n in range(bandit_count):
+        bandit = Bandit(10,epsilon)
+        for play in range(plays):
+            arm = bandit.get_arm() #Get arm to pull
+            reward = bandit.get_reward(arm) #Calculate Reward
+            bandit.update_estimate_values(arm,reward) #Update estimate values
+            avg_r = bandit.get_average_reward() #Find max reward
+            average_reward[play] += avg_r
+            if bandit.best_action(arm):
+                optimal_action[play] += 1
+        if n > 0 and n % 100 == 0:
+            print("Loop: ",n)
+    optimal_action /= bandit_count
+    average_reward /= bandit_count
+    return optimal_action, average_reward
+
+
+def kArmedEpsilonGreedy():
+    """Figure 2.1"""
+    tasks = 2000
+    plays = 1000
+
+    optimal_action00, average_reward00 = process_bandit(0, tasks, plays)
+    optimal_action01, average_reward01 = process_bandit(0.1, tasks, plays)
+    optimal_action001, average_reward001 = process_bandit(0.01, tasks, plays)
+    
+    plt.plot(average_reward00,label='E=0')
+    plt.plot(average_reward01,label='E=0.1')
+    plt.plot(average_reward001,label='E=0.01')
+    plt.xlabel("Plays")
+    plt.ylabel("Average reward")
+    plt.legend()
+    plt.show()
+
+    plt.plot(optimal_action00,label="e=0 (greedy)")
+    plt.plot(optimal_action01,label="e=0.1 (greedy)")
+    plt.plot(optimal_action001,label="e=0.01 (greedy)")
+    plt.xlabel("Plays")
+    plt.ylabel("% Optimal Action")
+    plt.legend()
+    plt.show()    
+
+
+if __name__ == "__main__":
+    kArmedEpsilonGreedy()
