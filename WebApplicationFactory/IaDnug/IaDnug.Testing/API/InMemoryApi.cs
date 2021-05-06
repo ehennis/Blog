2b193da184e5444c301e114510354944aaa93d55
Evan Hennis <evanhennis@gmail.com>
2021-05-06 08:51:11 -0500

    Added discount code and fixed the DbContext

--- WebApplicationFactory/IaDnug/IaDnug.Testing/API/InMemoryApi.cs
+++ WebApplicationFactory/IaDnug/IaDnug.Testing/API/InMemoryApi.cs
@@ -1,6 +1,7 @@
 ﻿using IaDnug.DataStore;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Identity.Client;
@@ -49,8 +50,13 @@ namespace IaDnug.Testing.API
                     //Will remove existing DB Context
                     services.AddDbContext<DbContext>(options =>
                     {
+                        //From Microsoft.EntityFrameworkCore.InMemory library
                         options.UseInMemoryDatabase("FileName");
+                        // Don't raise error about InMemory not supporting transactions
+                        options.ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+
                     });
+
 
                     //Add any other DI stuff here!
                     services.AddScoped<IDataStore, WeatherDataStoreInMemory>();
